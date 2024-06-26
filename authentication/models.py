c47d48ee5a8d672f3b35c022c7ff3e56c295ffb8
SrizanKhadka <khadkasrijan785@gmail.com>
2024-06-26 22:03:12 +0545

    user registration api is done.

--- authentication/models.py
+++ authentication/models.py
@@ -4,9 +4,10 @@ from django.contrib.auth.models import AbstractUser
 # Model for users.
 
 class UserModel(AbstractUser):
+    username = None
     role = models.CharField(max_length=20, default="user")
     fullName = models.CharField(max_length=20)
-    email = models.EmailField()
+    email = models.EmailField(unique=True)
     phoneNumber = models.CharField(max_length=15)
     gender = models.CharField(max_length=20)
     bloodGroup = models.CharField(max_length=20)
@@ -14,6 +15,10 @@ class UserModel(AbstractUser):
     address = models.CharField(max_length=100)
     password = models.CharField(max_length=18)
     confirmPassword = models.CharField(max_length=18)
+    
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['fullName', 'phoneNumber', 'gender', 'bloodGroup', 'dob', 'address']
+
 
     def __str__(self):
         return self.fullName
