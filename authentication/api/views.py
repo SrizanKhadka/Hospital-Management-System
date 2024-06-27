ff6d8f8beda371f5c105ef3d7fa70200a138f4b7
SrizanKhadka <khadkasrijan785@gmail.com>
2024-06-27 22:17:58 +0545

    login is done for users.

--- authentication/api/views.py
+++ authentication/api/views.py
@@ -1,11 +1,17 @@
 from rest_framework.views import APIView
 from authentication.models import UserModel
-from authentication.api.serializers import UserSerializer
+from authentication.api.serializers import UserSerializer, LoginSerializer
 from rest_framework import permissions
 from rest_framework.viewsets import ModelViewSet
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+
 
 
 class RegistrationView(ModelViewSet):
     serializer_class = UserSerializer
     queryset = UserModel.objects.all()
     permission_classes = [permissions.AllowAny]
+
+class LoginAPIView(TokenObtainPairView):
+    serializer_class = LoginSerializer
+    
